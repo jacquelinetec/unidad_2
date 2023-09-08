2ad6d64a2c490c9c3c2a23267879485041c6a6ca
jacquelinetec <143621961+jacquelinetec@users.noreply.github.com>
2023-09-08 08:39:51 -0600

    Add files via upload

---
+++ Practica_2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica_2;
+
+/**
+ *
+ * @author Jacqueline Sarabia
+ */
+public class Practica_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int base=3;
+        int altura=14;
+        int area= base*altura/2;
+        System.out.println("el resyltado es "+ area);
+    }
+    
+}
