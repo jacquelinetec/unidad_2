61054c8ca3f4ae7a1f8aefb75b1abdfd7b8bf077
jacquelinetec <143621961+jacquelinetec@users.noreply.github.com>
2023-09-08 08:46:30 -0600

    Add files via upload

---
+++ Clase_2.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clase_2;
+
+/**
+ *
+ * @author Jacqueline Sarabia
+ */
+public class Clase_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String tarea1= "darle de comer al perro";
+        String tarea2= "lavarme los dientes";
+        String tarea3= "bañarme";
+        String tarea4= "arreglarme";
+        String tarea5= "hacer tarea ";
+        String cadenatexto= "cadema de texto!!! %% $$$";
+        
+        System.out.println("la primera es " + tarea1);
+        System.out.println("la segunda es " + tarea2);
+        System.out.println("la tercera es " + tarea3);
+        System.out.println("la cuarta es " + tarea4);
+        System.out.println("la quinta es " + tarea5);
+    }
+    
+}
